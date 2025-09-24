417f3c3334127583c3aa8f6d4d5c3f390d8c6a8d
Ayelén Vallejo <112521947+ayevallejo@users.noreply.github.com>
2025-09-23 23:07:04 -0300

    agrego el continido vía carga de archivos

---
+++ app.js
@@ -0,0 +1,28 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const inputAmigo = document.getElementById ("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById ("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    if (!inputAmigo.value){
+        alert("Ingresá un nombre!")
+    }
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+
+};
+
+
+function sortearAmigo() {
+    const random = Math.floor(Math.random() * listaAmigos.length);
+    const amigoSecreto = listaAmigos[random];
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+    
+}
+
+
+
+
